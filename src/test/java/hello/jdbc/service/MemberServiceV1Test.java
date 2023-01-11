09ae14e48e1e4eceb89dc026dcc83861a7396a64
dilan-micehub <dilan@micehub.com>
2023-01-12 08:54:12 +0900

    0112-transaction-code

--- src/test/java/hello/jdbc/service/MemberServiceV1Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -3,7 +3,9 @@ package hello.jdbc.service;
 import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,15 +16,18 @@ import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * 기본 동작, 트랜잭션이 없어서 발생하는 문제 테스트
 * */
 public class MemberServiceV1Test {
 
+    //constant
     public static final String MEMBER_A = "memberA";
     public static final String MEMBER_B = "memberB";
     public static final String MEMBER_EX = "ex";
+    //
 
     private MemberRepositoryV1 memberRepositoryV1;
     private MemberServiceV1 memberService;
@@ -31,24 +36,33 @@ public class MemberServiceV1Test {
     void before() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         memberRepositoryV1 = new MemberRepositoryV1(dataSource);
+        memberService = new MemberServiceV1(memberRepositoryV1);
+    }
+
+    @AfterEach
+    void after() throws SQLException {
+        memberRepositoryV1.delete(MEMBER_A);
+        memberRepositoryV1.delete(MEMBER_B);
+        memberRepositoryV1.delete(MEMBER_EX);
+        //트랜잭션을 사용하게 된다면 굳이 전부 지우지 않고 assert만 찍어보고 commit을 하지 않고 rollback 시켜버리면 된다....!!!!
     }
 
     @Test
     @DisplayName("정상 이체")
-    void accountTransfer() throws SQLException {
+    void accountTransferEx() throws SQLException {
         //given
         Member memberA = new Member(MEMBER_A, 10000);
-        Member memberB = new Member(MEMBER_B, 10000);
+        Member memberEX = new Member(MEMBER_EX, 10000);
         memberRepositoryV1.save(memberA);
-        memberRepositoryV1.save(memberB);
+        memberRepositoryV1.save(memberEX);
 
         //when
-        memberService.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        assertThatThrownBy(() -> memberService.accountTransfer(memberA.getMemberId(), memberEX.getMemberId(), 2000));
 
         //then
         Member findMemberA = memberRepositoryV1.findById(memberA.getMemberId());
-        Member findMemberB = memberRepositoryV1.findById(memberB.getMemberId());
+        Member findMemberB = memberRepositoryV1.findById(memberEX.getMemberId());
         assertThat(findMemberA.getMoney()).isEqualTo(8000);
-        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+        assertThat(findMemberB.getMoney()).isEqualTo(10000);
     }
 }
